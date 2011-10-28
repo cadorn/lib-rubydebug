ed1ba76413b8735bc0323a0cd36f76667a0e1c0b
cadorn <christoph@christophdorn.com>
2011-10-28 14:41:59 -0700

    locked session test

--- test/session.js
+++ test/session.js
@@ -85,6 +85,52 @@ var Test =
         HELPER.runBrowserTest("session", function() {
             next();
         });
+    },
+
+    // A session that is locked to the client so no other connected client can monitor
+    // the session. This works by having the proxy server lock the session based on
+    // the session name (second argument to `HELPER.debugScript()` below).
+    "test lockedSession": function(next)
+    {
+        var client = new CLIENT.Client(HELPER.getClientOptions());
+
+        client.on("connect", function(data)
+        {
+        	HELPER.debugScript("HelloWorld", "session-locked");
+        });
+
+        client.on("session", function(session)
+        {
+            session.on("end", function(status)
+            {
+                client.disconnect();
+            });
+
+            // @issue https://github.com/ruby-debug/ruby-debug-ide/issues/9 (currently implemented by watching script output)
+            session.sendCommand("set", ["stdout", "1"], null, function(args, data, raw)
+            {
+				ASSERT.equal(args[0], "stdout");
+				ASSERT.equal(args[1], "1");
+
+                session.sendCommand("set", ["stderr", "1"], null, function(args, data, raw)
+                {
+    				ASSERT.equal(args[0], "stderr");
+    				ASSERT.equal(args[1], "1");
+
+    	        	// @see http://bashdb.sourceforge.net/ruby-debug.html#Continue
+    	        	session.sendCommand("cont");
+                });
+            });
+        });
+
+        client.on("disconnect", function(data)
+        {
+        	next();
+        });
+
+        client.connect({
+        	id: "client-server-session-locked"
+        });
     }
 }
 
